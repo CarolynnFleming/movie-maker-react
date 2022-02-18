47e37e9e32780fe28bb30b2d1a77f593acbca529
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 16:13:48 -0500

    props

---
+++ src/MovieList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Movie from './Movie';
+import WatchListItem from './WatchListItem';
+
+export default function MovieList() {
+  return (
+    <div>MovieList</div>
+  )
+}
---
+++ src/WatchListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { sawMovie } from './services/fetch_utils';
+
+
+export default function WatchListItem({ movie, refreshSawList }) {
+  return (
+    <div>WatchListItem</div>
+  )
+}
