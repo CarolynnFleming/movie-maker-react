7bce7b71900660e6bd511fb90c291d47a5c3c69d
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 16:05:33 -0500

    movie

--- src/Movie.js
+++ src/Movie.js
@@ -6,21 +6,31 @@ export default function Movie({
   isOnSawList,
   refreshSawList,
 }) {
-    const haveWatched = isOnSawList(movie.id);
+  const haveWatched = isOnSawList(movie.id);
 
-    async function handleClick() {
-        if (!haveWatched) {
-            const sawlistItem = {
-                title: movie.title,
-                api_id: movie.id,
-                description: movie.overview,
-                poster: movie.poster_path,
-            };
-            await addToMovieSearchList(sawlistItem);
-            await refreshSawList();
-        }
+  async function handleClick() {
+    if (!haveWatched) {
+      const sawlistItem = {
+        title: movie.title,
+        api_id: movie.id,
+        description: movie.overview,
+        poster: movie.poster_path,
+      };
+      await addToMovieSearchList(sawlistItem);
+      await refreshSawList();
     }
+  }
   return (
-    <div>Movie</div>
+    <div
+      title="movie-item"
+      onClick={handleClick}
+      className={`movie-item ${haveWatched ? 'watched' : ''}`}>
+      <h1>{haveWatched && '💖'}</h1>
+      <h3>{movie.title}</h3>
+      <em>{movie.overview}</em>
+      <p>
+        <img src={movie.poster_path ? `https://image.tmb.org/t/p/original${movie.poster_path}` : 'https://www.placebear.com/200/300'} />
+      </p>
+    </div>
   );
 }
