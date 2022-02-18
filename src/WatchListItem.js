c50705fa9d08fda1a2c0399bbf78133e3521c8c6
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 16:30:02 -0500

    movielist

--- src/WatchListItem.js
+++ src/WatchListItem.js
@@ -3,7 +3,20 @@ import { sawMovie } from './services/fetch_utils';
 
 
 export default function WatchListItem({ movie, refreshSawList }) {
+  async function handleClick() {
+    await sawMovie(movie.id);
+
+    await refreshSawList();
+  }
   return (
-    <div>WatchListItem</div>
-  )
+    <div
+      onClick={handleClick} className='movie-item watchlist-item'>
+      <h1>{movie.watched ? '✅' : '👀'}</h1>
+      <h3>{movie.title}</h3>
+      <em>{movie.description}</em>
+      <p>
+        <img src={movie.poster ? `https://image.tmdb.org/t/p/original${movie.poster}` : 'https://www.placebear.com/200/300'} />
+      </p>
+    </div>
+  );
 }
