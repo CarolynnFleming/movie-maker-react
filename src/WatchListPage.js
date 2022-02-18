d0c156ab1deb0623c8c54827577c736b6f0406d8
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 18:03:03 -0500

    refreshsawlist

--- src/WatchListPage.js
+++ src/WatchListPage.js
@@ -5,6 +5,11 @@ import { getMovieSearchList } from './services/fetch_utils';
 
 export default function WatchListPage() {
     const [movies, setmovies] = useState([]);
+    async function refreshSawList() {
+        const mySawList = await getMovieSearchList();
+
+        setmovies(mySawList);
+    }
   return (
     <div>WatchListPage</div>
   )
