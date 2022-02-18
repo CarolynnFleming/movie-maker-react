e6e1002aebd589249d90a5d8101eb67c7a1f6691
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 14:30:57 -0500

    changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -26,7 +26,18 @@ export default function HomePage({ setUser }) {
     setSignInPassword('');
   }
   return (
-    <div>HomePage</div>
+    <div className='home page'>
+        <form onSubmit={handleSignUp}>
+            <label>
+                Email
+                <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)} />
+            </label>
+            <label>
+                Password
+                <imput value={signUpPassword} type='password' onChange={e => setSignUpPassword(e.target.value)} />
+            </label>
+        </form>
+    </div>
   );
 }
 
--- src/services/fetch_utils.js
+++ src/services/fetch_utils.js
@@ -1,16 +1,16 @@
 import { client, checkError } from './client';
 
 export function getUser() {
-  return client.auth.session() && client.auth.session().user;
+  return client.auth.session();
 }
 
-export async function signUpUser(email, password) {
+export async function signUp(email, password) {
   const response = await client.auth.signUp({ email, password });
 
   return response.user;
 }
 
-export async function signInUser(email, password) {
+export async function signIn(email, password) {
   const response = await client.auth.signIn({ email, password });
 
   return response.user;
