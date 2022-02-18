45ba25fb61a6c6cc583ac48d0210cf09fdecc801
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 15:55:31 -0500

    handleclick

--- src/Movie.js
+++ src/Movie.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { addToMovieSearchList } from './services/fetch_utils';
 
 export default function Movie({
-    movie,
-    isOnSawList,
-    refreshSawList,
+  movie,
+  isOnSawList,
+  refreshSawList,
 }) {
+    const haveWatched = isOnSawList(movie.id);
+
+    async function handleClick() {
+        if (!haveWatched) {
+            const sawlistItem = {
+                title: movie.title,
+                api_id: movie.id,
+                description: movie.overview,
+                poster: movie.poster_path,
+            };
+            await addToMovieSearchList(sawlistItem);
+            await refreshSawList();
+        }
+    }
   return (
     <div>Movie</div>
-  )
+  );
 }
