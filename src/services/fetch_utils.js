6c3a4dff1e58a97c2edb1a068d1cbdef09cf6e2a
CarolynnFleming <cfcatpink@gmail.com>
2022-02-18 14:06:13 -0500

    fetch

--- src/services/fetch_utils.js
+++ src/services/fetch_utils.js
@@ -1,25 +1,50 @@
 import { client, checkError } from './client';
 
 export function getUser() {
-    return client.auth.session() && client.auth.session().user;
+  return client.auth.session() && client.auth.session().user;
 }
 
 export async function signUpUser(email, password) {
-    const response = await client.auth.signUp({ email, password });
+  const response = await client.auth.signUp({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
 export async function signInUser(email, password) {
-    const response = await client.auth.signIn({ email, password });
+  const response = await client.auth.signIn({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
 export async function logout() {
-    await client.auth.signOut();
+  await client.auth.signOut();
 
-    return (window.location.href = '../');
+  return (window.location.href = '../');
 }
 
+export async function addToMovieSearchList(movie) {
+  const response = await client
+    .from('movie-search')
+    .insert(movie);
 
+  return checkError(response);
+}
+
+export async function getMovieSearchList() {
+  const response = await client
+    .from('movie-search')
+    .select()
+    .order('id');
+
+  return checkError(response);
+}
+
+export async function sawMovie(id) {
+  const response = await client
+    .from('movie-search')
+    .update({ saw: true })
+    .match({ id })
+    .single();
+
+  return checkError(response);
+}
